2bc2f38df3a9eea1e71890f083315de82a15efff
김선형 <133829875+tjsgud99@users.noreply.github.com>
2024-03-16 17:44:33 +0900

    Update 01_openai_ai.py

--- 01_openai_ai.py
+++ 01_openai_ai.py
@@ -17,15 +17,23 @@
 # OpenAi 회사에서 GPT 관련 API 제공
 #   -https://openai.com/blog/openai-api
 
+
 from openai import OpenAI
-client = OpenAI(api_key="")
+client = OpenAI(api_key="sk-n6yGrUjUYOapaM7QHZo6T3Blbk")
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": ""},
-    {"role": "user", "content": "클라우드 설명해줘"}
+    {"role": "system", "content": "모든 설명을 3줄로 요약해서 설명해주세요."},
+    {"role": "user", "content": "클라우드 설명해줘"}  #zzzzz
   ]
 )
 
 print(completion.choices[0].message)
+
+
+# OpenAI의 API를 사용해서 챗봇 문제점
+# 1.개발이 어려움(난이도 상) -> 더 쉽게 개발할 수 있는 무언가(프레임워크) 필요!
+# 2.챗봇 개발 완성 -> Bard 모델 변경 -> Bard API 처음부터 개발 -> 프레임워크(LLM)
+
+# -> LangChain 프레임워크(코드 동일 : 모델 바꾸면)
